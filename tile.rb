9b838a97858d97a65cffcfd2feee92f1b6bad160
Spencer Y. Zhang <syz4@cornell.edu>
2014-11-18 18:41:19 -0600

    Implemented tileset loading

--- tile.rb
+++ tile.rb
@@ -2,12 +2,15 @@ require 'oily_png'
 require_relative "rpg.rb"
 
 module OHR
-  class Tile
+  class Tileset
 
-    def initialize rpg
+    def initialize rpg, id
       @rpg = rpg
     end
 
+    def palette
+      @palette ||= load_master_palette
+    end
     def load_master_palette
       file = File.binread("#{@rpg.filename}.rpgdir/palettes.bin")
       filesize = file.unpack("v*")[1]
@@ -15,8 +18,18 @@ module OHR
       (0..255).to_a.map{|i| ChunkyPNG::Color::rgb(palette[3*i], palette[3*i+1], palette[3*i+2]) }
     end
 
-    def load_tileset id
+    def tileset id
+      @tileset ||= File.binread("#{@rpg.filename}.rpgdir/#{@rpg.archinym}.til", 320*200, id*320*200).unpack("C*").map{|i| palette[i]}
+    end
 
+    def draw
+      png = ChunkyPNG::Image.new(320, 200)
+      (0..3).each do |phase|
+        (0...50*320).each do |i|
+          png[4*(i%80) + phase, i/80] = tileset(2)[50*320*phase + i]
+        end
+      end
+      png.save("tileset.png", interlace:true)
     end
 
   end
@@ -24,5 +37,5 @@ end
 
 rpg = OHR::RPG.new("spllshrd")
 
-tile = OHR::Tile.new(rpg)
-p tile.load_tileset 1
+tileset = OHR::Tileset.new(rpg, 1)
+tileset.draw
